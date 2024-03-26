e23dc2385cfa23ce632360389b1b3f63d3ae8f5a
KushalMDev <105456987+KushalMDev@users.noreply.github.com>
2024-03-26 15:06:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-//your JS code here. If required.
+let soundbtns=document.querySelectorAll(".btn")
+soundbtns.forEach((button)=>{
+    button.addEventListener("click",()=>{
+        let sid=button.dataset.sound
+        const audio=document.getElementById(sid)
+        if(audio){
+            document.querySelectorAll('audio').forEach(audio=>audio.pause());
+
+            audio.currentTime=0;
+            audio.play();
+        }
+    })
+})
+
+let stopbtn=document.querySelector(".stop")
+stopbtn.addEventListener("click",()=>{
+    document.querySelectorAll('audio').forEach(audio=>audio.pause());
+})
